3771c2aae3f000698f8253150aef4b9f64fa76eb
github username <github email address>
2017-02-11 13:42:52 -0500

    second commit, added the broad search conditional

--- lib/data_fetchers/nytreview_api_caller.rb
+++ lib/data_fetchers/nytreview_api_caller.rb
@@ -8,7 +8,7 @@ class ReviewAPI
       result = JSON.parse(RestClient.get(search_url).body)
       if result["results"].count == 1
         Model.new(result["results"][0]["display_title"],result["results"][0]["mpaa_rating"],result["results"][0]["critics_pick"],result["results"][0]["byline"],result["results"][0]["summary_short"],result["results"][0]["link"]["url"]).display_information
-      elsif result["results"].count > 1
+      elsif result["results"].count > 1 && !result["has_more"]
         puts "We found a few movies that matched your search."
           result["results"].each_with_index do |movie,index|
             puts "#{index + 1}. #{movie["display_title"]}"
@@ -17,8 +17,10 @@ class ReviewAPI
             user_input = gets.chomp.to_i
             user_input -= 1
             Model.new(result["results"][user_input]["display_title"],result["results"][user_input]["mpaa_rating"],result["results"][user_input]["critics_pick"],result["results"][user_input]["byline"],result["results"][user_input]["summary_short"],result["results"][user_input]["link"]["url"]).display_information
-      else
+        elsif
         puts "We could not find any matches, please try again"
+      else
+        puts "Your search is too broad. Please try again. "
       end
   end
 
