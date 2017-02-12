2da7eb9b9773539ff4d0491647d70810e91e4013
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-11 21:40:17 -0500

    Refined Critics' Picks & added review dates

--- lib/data_fetchers/nytreview_api_caller.rb
+++ lib/data_fetchers/nytreview_api_caller.rb
@@ -7,7 +7,7 @@ class ReviewAPI
     user_input = 0
       result = JSON.parse(RestClient.get(search_url).body)
       if result["results"].count == 1
-        Model.new(result["results"][0]["display_title"],result["results"][0]["mpaa_rating"],result["results"][0]["critics_pick"],result["results"][0]["byline"],result["results"][0]["summary_short"],result["results"][0]["link"]["url"]).display_information
+        Model.new(result["results"][0]["display_title"],result["results"][0]["publication_date"],result["results"][0]["mpaa_rating"],result["results"][0]["critics_pick"],result["results"][0]["byline"],result["results"][0]["summary_short"],result["results"][0]["link"]["url"]).display_information
       elsif result["results"].count > 1 && !result["has_more"]
         puts "We found a few movies that matched your search."
           result["results"].each_with_index do |movie,index|
@@ -16,11 +16,11 @@ class ReviewAPI
             puts "Please enter the number associated with the correct movie"
             user_input = gets.chomp.to_i
             user_input -= 1
-            Model.new(result["results"][user_input]["display_title"],result["results"][user_input]["mpaa_rating"],result["results"][user_input]["critics_pick"],result["results"][user_input]["byline"],result["results"][user_input]["summary_short"],result["results"][user_input]["link"]["url"]).display_information
+            Model.new(result["results"][user_input]["display_title"],result["results"][user_input]["publication_date"],result["results"][user_input]["mpaa_rating"],result["results"][user_input]["critics_pick"],result["results"][user_input]["byline"],result["results"][user_input]["summary_short"],result["results"][user_input]["link"]["url"]).display_information
         elsif
         puts "We could not find any matches, please try again"
       else
-        puts "Your search is too broad. Please try again. "
+        puts "Your search is too broad. Please try again."
       end
   end
 
--- lib/models/nytreview_model.rb
+++ lib/models/nytreview_model.rb
@@ -1,9 +1,10 @@
 class Model
 
-  attr_accessor :display_title, :rating, :critics_pick, :byline, :short_summary, :review_url
+  attr_accessor :display_title, :publication_date, :rating, :critics_pick, :byline, :short_summary, :review_url
 
-  def initialize(display_title,rating,critics_pick,byline,short_summary,review_url,number=0)
+  def initialize(display_title,publication_date,rating,critics_pick,byline,short_summary,review_url,number=0)
     @display_title = display_title
+    @publication_date = publication_date
     @rating = rating
     @critics_pick = critics_pick
     @byline = byline
@@ -13,17 +14,16 @@ class Model
   end
 
   def display_information
-    puts "Booyakasha" +  "\n" + "\n"
-    puts "Film: #{@display_title}" + "\n" + "\n"
-    puts "MPAA Rating: #{@rating}" + "\n" + "\n"
+    puts "Booyakasha" + "\n" + "\n"
+    puts "Film: #{@display_title}" + "\n"
+    puts "Review date: #{@publication_date}" + "\n"
+    puts "MPAA Rating: #{@rating}" + "\n"
     if @critics_pick == 1
-      puts "Critic Recommended" + "\n" + "\n"
-    else
-      puts "Not Critic Recommended" + "\n" + "\n"
+      puts "Critics' Pick" + "\n"
     end
-    puts "Reviewer: #{@byline}"  + "\n" + "\n"
-    puts "Synopsis: #{@short_summary}" + "\n" + "\n"
-    puts "Read more here: #{@review_url}" +"\n" + "\n"
+    puts "Reviewer: #{@byline}" + "\n"
+    puts "Synopsis: #{@short_summary}" + "\n"
+    puts "Read more here: #{@review_url}" + "\n" + "\n"
   end
 
 
